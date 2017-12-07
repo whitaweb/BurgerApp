6af15f55cbfe4b36a7669c6d783820751e913db0
whitaweb <26194993+whitaweb@users.noreply.github.com>
2017-12-07 13:38:20 -0600

    new files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy Bacon', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Super Vegan', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicago Style', 0);
